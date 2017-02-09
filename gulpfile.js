b7aa47912c63bc042a845c9298ff9cfd9dc082b4
C.wang <C.wang>
2017-02-09 14:51:57 +0800

    '第一次提交'

---
+++ gulpfile.js
@@ -0,0 +1,73 @@
+/**
+ * Created by 王敏 on 2017/2/8.
+ */
+	/*
+	1.less编译 压缩 合并
+	2.js合并 压缩 混淆
+	3.img复制
+	4.html压缩
+	*/
+//在gulpfile中先载入gulp包，因为这个包提供了一些API
+var gulp = require('gulp');
+var less = require('gulp-less');
+var cssnano = require('gulp-cssnano');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var htmlmin = require('gulp-htmlmin');
+
+//1.less编译 压缩 合并没有必要，一般预处理css都可以导包
+gulp.task('style',function(){
+	gulp.src(['src/style/*.less','!src/style/_*.less'])
+		.pipe(less())
+		.pipe(cssnano())
+		.pipe(gulp.dest('dist/style'))
+		.pipe(browserSync.reload({
+			stream: true
+		}));
+});
+//2.js合并 压缩混淆
+gulp.task('script',function(){
+	gulp.src('src/script/*.js')
+		.pipe(concat('all.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('dist/script'))
+		.pipe(browserSync.reload({
+			stream: true
+		}));
+});
+//3.图片复制
+gulp.task('image',function(){
+	gulp.src('src/images/*.*')
+		.pipe(gulp.dest('dist/images'))
+		.pipe(browserSync.reload({
+			stream: true
+		}));
+});
+//4.html压缩
+ gulp.task('html',function(){
+	 gulp.src('src/*.html')
+		 .pipe(htmlmin({
+			 collapseWhitespace:true,
+			 removeComments:true
+		 }))
+		 .pipe(gulp.dest('dist'))
+		 .pipe(browserSync.reload({
+			 stream: true
+		 }));
+ });
+var browserSync = require('browser-sync');
+gulp.task('serve', function() {
+	browserSync({
+		server: {
+			baseDir: ['dist']
+		}
+	}, function(err, bs) {
+		console.log(bs.options.getIn(["urls", "local"]));
+	});
+
+	gulp.watch('src/style/*.less',['style']);
+	gulp.watch('src/script/*.js',['script']);
+	gulp.watch('src/images/*.*',['image']);
+	gulp.watch('src/*.html',['html']);
+});
+
---
+++ src/script/demo.js
@@ -0,0 +1,5 @@
+/**
+ * Created by ÍõÃô on 2017/2/9.
+ */
+var c=10;
+alert(c);
